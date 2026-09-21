e957241908364e5628d5c87027769cde68bfa5bc
finance538 <finance@1shotcam.com>
2026-09-21 06:31:19 +0300

    fix: include current Wi-Fi subnet in allowedDevOrigins

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ["172.20.10.2", "localhost"],
+  allowedDevOrigins: [
+    "172.20.10.2",
+    "192.168.0.3",
+    "localhost",
+  ],
 };
 
 export default nextConfig;
